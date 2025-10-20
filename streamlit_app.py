27d4b8da867fbcea770c96ba4aaf721422ab301c
Jessica-git-trial <jessica.bonou@larochellegc.com>
2025-10-20 13:13:22 -0400

    Update streamlit_app.py
    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 #from snowflake.snowpark import session
 
@@ -42,6 +43,9 @@ if ingredients_list:
 
 for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ' '
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df =st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
   #  st.write (ingredients_string)
 
@@ -64,7 +68,7 @@ if time_to_insert:
 
 #NEW SECTION
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#import requests
+#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df =st.DATAFRAME(data = smoothiefroot_response.json(), use_container_width=True)
+#sf_df =st.DATAFRAME(data = smoothiefroot_response.json(), use_container_width=True)
